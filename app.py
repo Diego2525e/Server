129cbc92f6b4930b6c297cb9f6b0f0fcd5d6e65d
Diego <diegoaporterol@hotmail.com>
2019-06-18 23:41:58 -0500

    api

--- app.py
+++ app.py
@@ -8,6 +8,17 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
 mongo = PyMongo(app)
 
+api = Api(app, version='1.0', title='TodoMVC API',
+    description='A simple TodoMVC API',
+)
+
+ns = api.namespace('todos', description='TODO operations')
+
+todo = api.model('Todo', {
+    'id': fields.Integer(readOnly=True, description='The task unique identifier'),
+    'task': fields.String(required=True, description='The task details')
+})
+
 @app.route("/")
 def hello():
     return "Hello World!"
