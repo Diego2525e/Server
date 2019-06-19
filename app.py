7e4961c226f0c177cb7c6e8cdc615061c1a8b1df
Diego <diegoaporterol@hotmail.com>
2019-06-18 22:27:07 -0500

    mongo 1

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import request
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+mongo = PyMongo(app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000)
