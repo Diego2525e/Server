0beb016c2e3f69868fb16f6739ada84eb60146a1
Diego <diegoaporterol@hotmail.com>
2019-06-18 22:37:43 -0500

    hello word

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
 mongo = PyMongo(app)
 
+@app.route("/")
+def hello():
+    return "Hello World!"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000)
